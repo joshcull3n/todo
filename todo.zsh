16aae4362ed0055cc2fd7a51369e180e365d812f
joshcull3n <8888780+joshcull3n@users.noreply.github.com>
2022-10-04 09:07:26 -0700

    add done function

--- todo.zsh
+++ todo.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 # ensure tasks.txt exists
-FILE_DIR=./.todos
+FILE_DIR=~/.todos
 FILE_TASKS=$FILE_DIR"/tasks.txt"
 
 # first time #
@@ -14,36 +14,67 @@ fi
 # constants #
 REPEAT_CHAR=" ~"
 TODO_SYMBOL="·"
+DONE_SYMVOL="x"
 
-
-# prep vars #
 touch $FILE_TASKS
-TASKS=$(<$FILE_TASKS)
-TASKS_ARR=("${(f)TASKS}")
-TASK_DISPLAY_NAMES_ARR=()
-TOTAL_TASKS=1
-WINDOW_WIDTH=$(tput cols)
-WINDOW_HEIGHT=$(tput lines)
-
 
 
 # functions #
 addTask () {
-  echo -n $TOTAL_TASKS"|"$1"|NEW|\n" >> $FILE_TASKS
-  echo added task \'$*\' as task number $TOTAL_TASKS!
+  # add task to new tasks array
+  NEW_TASKS+=$1
+  rectifyTasks
+  listTasks
+}
+
+# takes number as input
+completeTask () {
+  doneTask=$NEW_TASKS[$1]
+  DONE_TASKS+=$doneTask 
+  NEW_TASKS=("${NEW_TASKS[@]:0:$1-1}" "${NEW_TASKS[@]:$1}") # remove from new task array
+  rectifyTasks
+  listTasks
+}
+
+# change task statuses in save file
+rectifyTasks () {
+  # clear file
+  clearTaskFile
+  
+  # rebuild file with current new tasks
+  for task in $NEW_TASKS;
+  do
+    echo -n $task"|NEW|\n" >> $FILE_TASKS
+  done 
+  
+  # add done tasks to file
+  for task in $DONE_TASKS;
+  do
+    echo -n $task"|DONE|\n" >> $FILE_TASKS
+  done
+}
+
+clearTaskFile () {
+  clear
+  truncate -s 0 $FILE_TASKS
 }
 
 listTasks () {
-  echo ""
+  clear
+  initTasks
   fillWidthChars " > todos"
   if [[ $TOTAL_TASKS -eq 1 ]]; then
     echo "\n   you don't have any todos!\n"
   fi
-  for taskName in $TASK_DISPLAY_NAMES_ARR
+  for taskName in $NEW_TASKS_DISPLAY
   do
-    echo "  "$taskName
+    echo " "$taskName
   done
-  echo ""
+  #fillWidthChars " > dones"
+  for taskName in $DONE_TASKS_DISPLAY
+  do 
+    echo " "$taskName
+  done
 }
 
 # first argument is input string
@@ -59,19 +90,45 @@ fillWidthChars () {
 resizeWindow () {
   clear
   printf '\e[8;51;64t'
+  listTasks
 }
 
-setVars () {
+initTasks () {
   TASKS=$(<$FILE_TASKS)
   TASKS_ARR=("${(f)TASKS}")
+  WINDOW_WIDTH=$(tput cols)
+  WINDOW_HEIGHT=$(tput lines)
+
+  NEW_TASKS=()
+  NEW_TASKS_COUNT=0
+  DONE_TASKS=()
+  DONE_TASKS_COUNT=0
+  NEW_TASKS_DISPLAY=()
+  DONE_TASKS_DISPLAY=()
+
+  for task in $TASKS_ARR; do
+    parsedTask=("${(@s/|/)task}")
+    if [[ $task == *"|NEW"* ]]; then
+      NEW_TASKS+="$parsedTask[1]"
+    elif [[ $task == *"|DONE"* ]]; then
+      DONE_TASKS+="$parsedTask[1]"
+    fi
+  done
   
-  # parse task name from each task and update total num
-  TASK_DISPLAY_NAMES_ARR=()
-  for task in $TASKS_ARR
+  NEW_TASKS_COUNT=${#NEW_TASKS[@]}
+  DONE_TASKS_COUNT=${#DONE_TASKS[@]}
+
+  count=0
+  for task in $NEW_TASKS;
   do
-    ((TOTAL_TASKS++))
-    parseString=("${(@s/|/)task}")
-    TASK_DISPLAY_NAMES_ARR+=" ["$parseString[1]"] $TODO_SYMBOL "$parseString[2]
+    parsedTask=("${(@s/|/)task}")
+    NEW_TASKS_DISPLAY+="$((count+=1))  $TODO_SYMBOL $parsedTask[1]"
+  done
+
+  for task in $DONE_TASKS;
+  do
+    parsedTask=("${(@s/|/)task}")
+    DONE_TASKS_DISPLAY+="$((count+=1))  $DONE_SYMVOL $parsedTask[1]"
   done
 }
 
@@ -80,7 +137,7 @@ help () {
 }
 
 # refresh variables every time the script is run
-setVars
+initTasks
 
 # commands #
 if [[ $# -ne 0 ]]; then
@@ -88,9 +145,12 @@ if [[ $# -ne 0 ]]; then
   shift
   case $COMMAND in
     add|a) addTask "$*" ;; # add task to todo list
-    list|l) listTasks  ;;  # list tasks 
-    resize) resizeWindow ;; # resize to ideal todo list size
+    list|l) listTasks ;; # list tasks
+    resize) resizeWindow ;; # resize window to ideal todo list size
+    done|d) completeTask "$*";; # mark task as complete
+    clear) clearTaskFile ;;
   esac
 else
+  clear
   listTasks
 fi
